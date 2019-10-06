9deb8a048e07f94593f25b1704eceb271ade7599
Misky Abshir <Misky.Abshir@gmail.com>
2019-10-05 23:59:22 -0500

    seperated edit page from the detail page

--- src/components/EditPage/EditPage.js
+++ src/components/EditPage/EditPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
@@ -6,15 +7,48 @@ import Button from '@material-ui/core/Button';
 
 class Edit extends Component {
   state={
-    title:'',
+    movie:{
+      id:'',
+      title:'',
+      description:'',
+    }
   }
 
-  handleChange = (event) =>{
+  componentDidMount = () =>{
+    this.movieEdit()
+  }
+
+  movieEdit = () =>{
+    this.props.reduxState.movies.map((movie) =>{
+      this.setState({
+        movie:{
+          id: movie.id,
+          title: movie.title,
+          description: movie.description
+        }
+      })
+      console.log('in movies ' ,movie);
+      
+    })
+  }
+
+  handleChange = (event, propertyNumber) =>{
     console.log(event.target.value);
         
     this.setState({
-        title:event.target.value
+        ...this.state.movie,
+        [propertyNumber]: event.target.value,
     })
+  }
+
+  handleSave = () =>{
+    console.log('in handleSave');
+    this.props.dispatch({type:'UPDATE_MOVIE', payload: this.movie})
+    this.props.history.push(`/movies/${this.props.match.params.id}`)    
+  }
+
+  handleCancel = () =>{
+    this.props.history.push(`/movies/${this.props.match.params.id}`)    
   }
   render() {
     return (
@@ -22,7 +56,7 @@ class Edit extends Component {
          <TextField
         id="outlined-uncontrolled"
         label="Title"
-        onChange={this.handleChange}
+        onChange={(event) => this.handleChange(event, 'title')}
         margin="normal"
         variant="outlined"
       />
@@ -32,16 +66,18 @@ class Edit extends Component {
         label="Description"
         multiline
         rowsMax="4"
-        onChange={this.handleChange}
+        onChange={(event) => this.handleChange(event, 'description')}
         margin="normal"
         variant="outlined"
       />
       <br/>
-      <Button>Save</Button>
-      <Button>Cancel</Button>
+      <Button onClick={this.handleSave}>Save</Button>
+      <Button onClick={this.handleCancel}>Cancel</Button>
       </div>
     );
   }
 }
-
-export default Edit;
+const mapStateToProps = (reduxState) =>({
+reduxState,
+})
+export default connect(mapStateToProps)(Edit);
